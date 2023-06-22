6770e76808f541c65a3c89d73ee96a556ead7e29
Cyro56 <cyro.costa@ztech.net>
2023-06-22 16:28:18 -0300

    config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
